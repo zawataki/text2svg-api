ffeeda65be7fb9fd1d69c3e5a8c02414de2c9a9c
Yuki Takizawa <ts-yuki.takizawa@rakuten.com>
2018-11-05 14:02:36 +0900

    Return message

--- src/main/java/com/example/helloworld/DemoApplication.java
+++ src/main/java/com/example/helloworld/DemoApplication.java
@@ -2,11 +2,21 @@ package com.example.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home(){
+		return "Hello World!";
 	}
 }
