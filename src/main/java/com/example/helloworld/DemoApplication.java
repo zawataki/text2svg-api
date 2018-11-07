ddb41d396150ab88fbad1271d6d3d67b22d4eb51
Yuki Takizawa <ts-yuki.takizawa@rakuten.com>
2018-11-07 17:10:06 +0900

    Return svg strings

--- src/main/java/com/example/helloworld/DemoApplication.java
+++ src/main/java/com/example/helloworld/DemoApplication.java
@@ -3,16 +3,11 @@ package com.example.helloworld;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.stream.Collectors;
 
 @Controller
 @SpringBootApplication
@@ -27,11 +22,19 @@ public class DemoApplication {
 
     @GetMapping("/")
     @ResponseBody
-    String textToSvg(@RequestParam String url) throws IOException {
+    String textToSvg(@RequestParam String url) {
 
-        final Resource resource = resourceLoader.getResource("classpath:/static/sample.svg");
-        final String fileContents = Files.lines(resource.getFile().toPath()).collect(Collectors.joining());
+        final String svg = "<svg xmlns=\"http://www.w3.org/2000/svg\"\n" +
+                "     width=\"500\" height=\"40\" viewBox=\"0 0 500 40\">\n" +
+                "\n" +
+                "    <rect fill=\"#fff\" id=\"canvas_background\" width=\"500\" height=\"40\" y=\"0\"\n" +
+                "          x=\"0\"/>\n" +
+                "\n" +
+                "    <text x=\"0\" y=\"35\" font-family=\"Verdana\" font-size=\"35\">\n" +
+                "        Hello, World\n" +
+                "    </text>\n" +
+                "</svg>\n";
 
-        return fileContents;
+        return svg;
     }
 }
