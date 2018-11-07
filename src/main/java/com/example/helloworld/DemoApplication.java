934e33a15d688376705ae070a2035e0dc2482cf3
Yuki Takizawa <ts-yuki.takizawa@rakuten.com>
2018-11-07 14:55:54 +0900

    Support only GET method

--- src/main/java/com/example/helloworld/DemoApplication.java
+++ src/main/java/com/example/helloworld/DemoApplication.java
@@ -3,7 +3,7 @@ package com.example.helloworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,13 +11,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	@RequestMapping("/")
-	@ResponseBody
-	String home(@RequestParam String url){
-		return "Hello World! with url: " + url;
-	}
+    @GetMapping("/")
+    @ResponseBody
+    String home(@RequestParam String url) {
+        return "Hello World! with url: " + url;
+    }
 }
