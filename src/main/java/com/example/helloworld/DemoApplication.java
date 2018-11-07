b398c697aba8d6167cea78ac40cf1689228c6cd4
Yuki Takizawa <ts-yuki.takizawa@rakuten.com>
2018-11-07 14:51:22 +0900

    Add query parameter named 'url'

--- src/main/java/com/example/helloworld/DemoApplication.java
+++ src/main/java/com/example/helloworld/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -16,7 +17,7 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	@ResponseBody
-	String home(){
-		return "Hello World!";
+	String home(@RequestParam String url){
+		return "Hello World! with url: " + url;
 	}
 }
