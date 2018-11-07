895e33a932bd90cddd03348855d8f9faf8829b9b
Yuki Takizawa <ts-yuki.takizawa@rakuten.com>
2018-11-07 15:40:07 +0900

    Returns svg file contents

--- src/main/java/com/example/helloworld/DemoApplication.java
+++ src/main/java/com/example/helloworld/DemoApplication.java
@@ -1,15 +1,26 @@
 package com.example.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
 @Controller
 @SpringBootApplication
 public class DemoApplication {
+
+    @Autowired
+    protected ResourceLoader resourceLoader;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
@@ -17,7 +28,11 @@ public class DemoApplication {
 
     @GetMapping("/")
     @ResponseBody
-    String home(@RequestParam String url) {
-        return "Hello World! with url: " + url;
+    String textToSvg(@RequestParam String url) throws IOException {
+
+        final Resource resource = resourceLoader.getResource("classpath:" + Paths.get("static", "sample.svg"));
+        final String fileContents = Files.lines(resource.getFile().toPath()).collect(Collectors.joining());
+
+        return fileContents;
     }
 }
