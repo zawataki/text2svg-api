670c3b1c33519ce320a7ecc74a27f5c58136c54e
Yuki Takizawa <y.takizawa64@gmail.com>
2018-11-13 00:14:27 +0900

    Refactoring

--- src/main/java/com/github/zawataki/text2svgapi/Text2SvgApiApplication.java
+++ src/main/java/com/github/zawataki/text2svgapi/Text2SvgApiApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Text2svgApiApplication {
+public class Text2SvgApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Text2svgApiApplication.class, args);
+		SpringApplication.run(Text2SvgApiApplication.class, args);
 	}
 }
--- src/test/java/com/github/zawataki/text2svgapi/Text2SvgApiApplicationTests.java
+++ src/test/java/com/github/zawataki/text2svgapi/Text2SvgApiApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Text2svgApiApplicationTests {
+public class Text2SvgApiApplicationTests {
 
 	@Test
 	public void contextLoads() {
