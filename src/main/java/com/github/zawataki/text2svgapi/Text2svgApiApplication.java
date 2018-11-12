caf27d7fad7c8ef6dfeb03a7323f9675f27c2e43
Yuki Takizawa <y.takizawa64@gmail.com>
2018-11-12 22:50:42 +0900

    Change build tool from Maven to Gradle

---
+++ src/main/java/com/github/zawataki/text2svgapi/Text2svgApiApplication.java
@@ -0,0 +1,12 @@
+package com.github.zawataki.text2svgapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Text2svgApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Text2svgApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/zawataki/text2svgapi/Text2svgApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.zawataki.text2svgapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Text2svgApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
